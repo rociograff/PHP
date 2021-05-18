db10a615a7a8acb2e69ba8b029980c219e36af45
Rocio <39743523+rociograff@users.noreply.github.com>
2021-05-18 00:57:23 -0300

    18/05/2021

---
+++ PEyLW/TPJS1/ejercicio3/ejercicio3a.js
@@ -0,0 +1,10 @@
+function variables() {
+    //aca defino las variables de los datos personales
+    var nombre = "Juan";
+    var apellido = "Perez";
+    var estadocivil = "Soltero";
+    var edad = 44;
+    var fechanacimiento = "1/1/1980";
+    var dni = 33333333;
+    document.write(nombre + "<br>" + apellido + "<br>" + estadocivil + "<br >" + edad + "<br>" + fechanacimiento + "<br>" + dni + "<br>")
+}
