129781c1de0765c1bdc0698773346e6db61e4922
Rocio <39743523+rociograff@users.noreply.github.com>
2021-06-14 16:32:01 -0300

    14/06/2021

--- PEyLW/TPJS4/ejercicio12/EjJavaScript012.js
+++ PEyLW/TPJS4/ejercicio12/EjJavaScript012.js
@@ -1,21 +1,21 @@
 function DOM(objeto){
     switch (objeto.nodeType){
         case 1: //Element
-        //cambia el estilo del objeto para que se visualice
-        objeto.style.backgroundColor="silver";
-        //se mustra un alert con tagName y innerHTML
-        alert("Elemento Node type:"+objeto.nodeType+"\nTag name:"+objeto.tagName+"\n"+objeto.innerHTML);
-        //se vuelve al color default
-        objeto.style.backgroundColor="";
-        //le asigno a la variable a el arreglo de nodos hijos
-        var a=objeto.childNodes
-        //para cada uno de los nodos hijos ejecuto la función DOM
-        for(i in a)
-            DOM(a[i]);
+            //cambia el estilo del objeto para que se visualice
+            objeto.style.backgroundColor="silver";
+            //se mustra un alert con tagName y innerHTML
+            alert("Elemento Node type:"+objeto.nodeType+"\nTag name:"+objeto.tagName+"\n"+objeto.innerHTML);
+            //se vuelve al color default
+            objeto.style.backgroundColor="";
+            //le asigno a la variable 'a' el arreglo de nodos hijos
+            var a=objeto.childNodes
+            //para cada uno de los nodos hijos ejecuto la función DOM
+            for(i in a)
+                DOM(a[i]);
         break;
         case 3: //Text
-        //Se muestra un alert con la propiedad nodeValue
-        alert("Texto Node type:"+objeto.nodeType+"\nnode Value:"+objeto.nodeValue);
+            //Se muestra un alert con la propiedad nodeValue
+            alert("Texto Node type:"+objeto.nodeType+"\nnode Value:"+objeto.nodeValue);
         break;
     }
 }
