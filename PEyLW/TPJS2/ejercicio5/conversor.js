5f029734d312e6cde1836306e0546e149ed5d9ea
Rocio <39743523+rociograff@users.noreply.github.com>
2021-06-14 23:34:37 -0300

    14/06/2021

--- PEyLW/TPJS2/ejercicio5/conversor.js
+++ PEyLW/TPJS2/ejercicio5/conversor.js
@@ -4,36 +4,36 @@ function conversor() {
         "ingrese: \n 1 para peso a dolar \n 2 para dolar a peso \n 3 para pesos a reales \n 4 reales a peso \n 5 pesos a euros \n 6 euros a pesos"
     );
     opcion = prompt("Introduce una opcion ", 0);
-    valor = prompt("Introduce un valor ", 0);
+    valor = prompt("Introduce un valor a convertir ", 0);
     switch (opcion) {
         case "1":
             document.write("peso a dolar <br>");
-            (resultado = valor / 14), 13;
+            (resultado = valor / 158);
             document.write(resultado);
         break;
         case "2":
             document.write("dolar a peso <br>");
-            (resultado = valor * 14), 13;
+            (resultado = valor * 158);
             document.write(resultado);
         break;
         case "3":
-            document.write("pesos a relaes <br>");
-            resultado = valor / 4.21;
+            document.write("pesos a reales <br>");
+            resultado = valor / 18.83;
             document.write(resultado);
         break;
         case "4":
             document.write("reales a pesos <br>");
-            resultado = valor * 4.21;
+            resultado = valor * 18.83;
             document.write(resultado);
         break;
         case "5":
             document.write("pesos a euros <br>");
-            resultado = valor / 16.92;
+            resultado = valor / 115.44;
             document.write(resultado);
         break;
         case "6":
             document.write("euros a pesos <br>");
-            resultado = valor * 16.92;
+            resultado = valor * 115.44;
             document.write(resultado);
         break;
         default:
